d241de8ab67ffd8759368117f8ef8971a74a8dde
Holger Zickner <hzickner@gmx.net>
2017-03-08 16:49:49 +0100

    print N/8 primes with timing

--- ASM/6502/examples/sieve.asm
+++ ASM/6502/examples/sieve.asm
@@ -54,20 +54,21 @@
 	sty u16_i
 	lda #0
 	sta u16_i+1			; i=2
-while1					; while(i<=st)
+	sta u16_count
+	sta u16_count+1			; count=0
+while1					; while(i<=N)
 	lda u16_N
 	cmp u16_i
 	lda u16_N+1
 	sbc u16_i+1
-	bcc while1out		; i>st
+	bcc while1out			; i>st
 
-	;ldy u16_i
-	lda (PTR2),Y		; flag[i]
+	lda (PTR2),Y			; flag[i]
 	bne else1
-						; if !flag[i]
+					; if !flag[i]
 	inc u16_count
 	bne @+
-	inc u16_count+1		; count++
+	inc u16_count+1			; count++
 @	tya 				;lda u16_i
 	asl
 	sta u16_k
@@ -85,7 +86,7 @@ while2
 	cmp u16_k
 	lda u16_N+1
 	sbc u16_k+1
-	bcc while2out		; while (k<=n)
+	bcc while2out			; while (k<=n)
 
 	lda PTR3
 	clc
@@ -118,13 +119,87 @@ while1out
 			
 	lda 20
 	ldx 19				; read calc time
+	sta u16_tm2
+	stx u16_tm2+1
+
+;
+; print primes < N2
+	ldy #flagbuf/256
+	ldx #flagbuf&$FF
+	bne @+
+	dey
+@	dex
+	stx PTR2
+	sty PTR2+1			; PTR2=flagbuf-1
+	
+	lda u16_N
+	sta u16_N2
+	lda u16_N+1
+	sta u16_N2+1
+
+	lsr u16_N2+1
+	ror u16_N2
+	lsr u16_N2+1
+	ror u16_N2
+	lsr u16_N2+1
+	ror u16_N2			; N2 >>= 3
+	
+	ldy #2
+	sty u16_i
+	lda #0
+	sta u16_i+1			; i=2
+	sta u16_count2
+	sta u16_count2+1		; count2=0
+while3					; while(i<=N2)
+	lda u16_N2
+	cmp u16_i
+	lda u16_N2+1
+	sbc u16_i+1
+	bcc while3out			; i>st
+
+	ldy u16_i
+	lda (PTR2),Y			; flag[i]
+	bne else3
+					; if !flag[i]
+	inc u16_count2
+	bne @+
+	inc u16_count2+1		; count2++
+@	lda u16_i
+	ldx u16_i+1
+	jsr printu16
+	lda #' '
+	jsr putc			; print i
+
+else3
+	inc u16_i
+	bne while3
+	inc u16_i+1			; i++; PTR1++
+	inc PTR2+1
+	bne while3			; end while3
+while3out
+	lda #CR
+	jsr putc
+
+	lda 20
+	ldx 19				; read print time
+	sta u16_tm4
+	stx u16_tm4+1
+	sec
+	sbc u16_tm2
+	sta u16_tm3
+	txa
+	sbc u16_tm2+1
+	sta u16_tm3+1			; tm3-=tm2
+	lda u16_tm2
 	sec
 	sbc u16_tm1
 	sta u16_tm2
-	txa
+	lda u16_tm2+1
 	sbc u16_tm1+1
-	sta u16_tm2+1
-
+	sta u16_tm2+1			; tm2-=tm1
+	
+;
+; print timings	
 	lda u16_N
 	ldx u16_N+1			; print N
 	jsr printu16
@@ -135,7 +210,7 @@ while1out
 	jsr putstring
 	lda u16_tm1
 	ldx u16_tm1+1
-	jsr printu16		; print tm memset
+	jsr printu16			; print tm memset
 	lda #STR2&$FF
 	sta PTR1
 	lda #STR2/256
@@ -144,7 +219,7 @@ while1out
 				
 	lda u16_count
 	ldx u16_count+1
-	jsr printu16		; print count
+	jsr printu16			; print count
 	lda #STR1&$FF
 	sta PTR1
 	lda #STR1/256
@@ -152,12 +227,37 @@ while1out
 	jsr putstring
 	lda u16_tm2
 	ldx u16_tm2+1
-	jsr printu16		; print tm prime calc
+	jsr printu16			; print tm prime calc
 	lda #STR2&$FF
 	sta PTR1
 	lda #STR2/256
 	sta PTR1+1
 	jsr putstring
+	
+	lda u16_count2
+	ldx u16_count2+1
+	jsr printu16			; print count
+	lda #STR1&$FF
+	sta PTR1
+	lda #STR1/256
+	sta PTR1+1
+	jsr putstring
+	lda u16_tm3
+	ldx u16_tm3+1
+	jsr printu16			; print tm prime calc
+	lda #STR2&$FF
+	sta PTR1
+	lda #STR2/256
+	sta PTR1+1
+	jsr putstring
+	lda u16_tm4
+	ldx u16_tm4+1
+	jsr printu16			; print tm prime calc
+	lda #STR4&$FF
+	sta PTR1
+	lda #STR4/256
+	sta PTR1+1
+	jsr putstring		
 	
 le	jmp le	
 	rts
@@ -196,17 +296,20 @@ CIOV   =   $E456
 
 ; floating point package
 FR0		= $D4	; 6 byte FP register
-INBUFF	= $F3	; 2 byte pointer to input buffer
-FASC	= $D8E6	; floating point to ATASCII, LBUFF = (string)FR0, INBUFF = last char
+INBUFF		= $F3	; 2 byte pointer to input buffer
+FASC		= $D8E6	; floating point to ATASCII, LBUFF = (string)FR0, INBUFF = last char
 IFP		= $D9AA ; 16bit integer in FR0 to floating point in FR0 
 
 STR1		.byte ' PRIMES IN',CR
 STR2		.byte ' TICKS',CR
 STR3		.byte ' BYTES CLEARED IN',CR
-u16_N		= $94
-u16_count	= $92
-u16_i		= $90
+STR4		.byte ' TICKS TOTAL',CR
 u16_k		= $88
+u16_i		= $90
+u16_count	= $92
+u16_N		= $94
+u16_N2		= $96
+u16_count2	= $98
 
 ; variables not initialized
 .align $100
@@ -214,7 +317,7 @@ flagbuf		.DS	NN
 u16_tm1		.DS	2
 u16_tm2		.DS	2
 u16_tm3		.DS	2
-
+u16_tm4		.DS	2
 	
 	run main	; start address
 	
